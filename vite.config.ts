0d0c2897411b852d6efd3743a06d6c53871afe07
Jeronimo Chacon Alzate <jerochaconalzate@gmail.com>
2026-04-14 18:41:31 -0500

    chore: set base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Wolf-Fitness-Personal-Trainer-Prototype/', // <--- Añade esta línea
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
